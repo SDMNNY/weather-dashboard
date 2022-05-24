e45ecd00659e1b5dedf84b88e6276369146aab39
sdmnny <sdmnny@gmail.com>
2022-05-23 21:06:50 -0400

    changed script vars

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,24 +1,15 @@
-function initWeather () {
-    const cityEl = document.getElementById("enter-city");
-    const searchEl = document.getElementById("search-button");
-    const clearEl = document.getElementById("clear-history");
-    const nameEl = document.getElementById("city-name");
-    const currentPicEl = document.getElementById("current-pic");
-    const currentTempEl = document.getElementById("temperature");
-    const currentHumidityEl = document.getElementById("humidity");
-    const currentWindEl = document.getElementById("wind-speed");
-    const currentUVEl = document.getElementById("UV-index");
-    const historyEl = document.getElementById("history");
-    var fivedayEl = document.getElementById("5day-header");
-    var todayweatherEl = document.getElementById("today-weather");
-    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+var searchButton = document.getElementById("search-button");
+var today = moment().format("1");
+function searchWeatherConditions () {
+    var search = document.getElementById("enter-city");
 
-    // API key
-    const APIKey = "ed95dbbfaa5c8b8a01c87da3afa2c95d";
+var weatherUrl =`https://api.openweathermap.org/data/2.5/weather?q=` +
+search.value +
+`&appid=ed95dbbfaa5c8b8a01c87da3afa2c95db&units=imperial`;
 
-    function getWeatherData(cityName) {
-        
-    }
+$
+
+
 
 
 
