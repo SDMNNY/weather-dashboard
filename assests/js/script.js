98b689e637b67ef310964b9f199e344385f1ed37
sdmnny <sdmnny@gmail.com>
2022-05-23 21:20:03 -0400

    editing script

--- assests/js/script.js
+++ assests/js/script.js
@@ -7,10 +7,13 @@ var weatherUrl =`https://api.openweathermap.org/data/2.5/weather?q=` +
 search.value +
 `&appid=ed95dbbfaa5c8b8a01c87da3afa2c95db&units=imperial`;
 
-$
+$("#5day-header").text("");
 
-
-
-
-
-}
+fetch(weatherUrl).then(function (response) {
+    if(response.ok){
+        $("#clear-history").append(
+            ${search.value}
+        );
+    return response.json().then(function (data) { 
+        $
+    }
